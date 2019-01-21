b521c1e37fec9ee654420a64eca600a2a5e33f89
andrew u frank <frank@santafe.local>
2019-01-21 17:16:23 +0100

    server cleaned does bake first

--- src/serverSG.hs
+++ src/serverSG.hs
@@ -2,7 +2,7 @@
 ------------------------------------------------------------------------------
 --
 -- Module      :   serving the baked site
---
+        -- started from treetagger where i used it with post -
 
 -----------------------------------------------------------------------------
 {-# LANGUAGE FlexibleContexts      #-}
@@ -19,22 +19,29 @@ module Main
 import Uniform.Strings
 import Uniform.Filenames
 import Uniform.FileStrings
+import Uniform.Error
+import Uniform.Convenience.StartApp
 
 import Web.Scotty
 import Network.Wai.Middleware.Static
 
 import Lib.Foundation
+import Lib.Bake (bake)
 
-bakedPort = 3000
+programName = "SSG" :: Text
+progTitle = unwords' ["constructing a static site generator"
+                , "on port ", showT bakedPort]  :: Text
+
+bakedPort = 3099
 
 main :: IO ()
 --main = quickHttpServe site
-main = do
-    putIOwords ["serverSG started", "for dir", showT bakedPath]
-    scotty bakedPort site
+main = startProg programName progTitle
+        $ do
+            bake
+            putIOwords ["serverSG started", "for dir", showT bakedPath, "on port", showT bakedPort, "\n"]
+            callIO $ scotty bakedPort site
 
---  middleware (static )) -- serves the source directory
---  notFound (text "404: Not found!")
 
 site :: ScottyM  ()
 -- for get, return the page from baked
@@ -42,21 +49,7 @@ site :: ScottyM  ()
 site = do
     get "/" $ file landingPage  -- showLandingPage
     middleware $ staticPolicy $ addBase (toFilePath bakedPath)
---    does not open the index for /
--- should the baked be included or not - included or not in relative path
 
---    post "/" showNoPostPage
---    delete "/" showNoPostPage
---    put "/" showNoPostPage
---    post "/test" ttgermanTest
--- test with : curl -d "Das ist ein einfacher Satz." localhost:17701/test
-{- result is
-TTdata {ttwf = "Das", ttpos = "PDS", ttlemma = "die"}
-TTdata {ttwf = "ist", ttpos = "VAFIN", ttlemma = "sein"}
-TTdata {ttwf = "ein", ttpos = "ART", ttlemma = "eine"}
-TTdata {ttwf = "einfacher", ttpos = "ADJA", ttlemma = "einfach"}
-TTdata {ttwf = "Satz.", ttpos = "NN", ttlemma = "<unknown>"}
--}
 
 landingPage = toFilePath $ addFileName bakedPath (makeRelFile "index.html")
 
@@ -67,36 +60,6 @@ showLandingPage   = do
 --  let x = " Sdsf" :: _
   html . t2tl . s2t  $ txt
 
---showBakedPage :: ActionM ()
----- get the page asked for
---showBakedPage = do
-
-
---ttgermanTest :: ActionM ()
----- body are tokens separated by blanks, to be easy to test
----- real thing is tokens separated by newline
---ttgermanTest  = do
---  sent  <- body
---  let sentT = bb2t . BL.toStrict $ sent
---  let toks = words' sentT :: [Text]
---  putIOwords ["ttgermanTest sentT:", sentT]
---  putIOwords toks
---  r1 <- liftIO (runErr $ ttProcess German toks  )
---  let res2 =   either (const zero)  (id) r1
---  putIOwords ["ttgermanTest res2:", res2]
---  text (L.fromStrict res2)
---
---ttgerman :: ActionM ()
---ttgerman  = do
---  sent  <- body
---  let sentT = bb2t . BL.toStrict $ sent
---  let toks = lines' sentT :: [Text]
---  putIOwords ["ttgerman sentT:", sentT]
---  putIOwords toks
---  r1 <- liftIO (runErr $ ttProcess German toks  )
---  let res2 =   either (const zero)  (id) r1
---  putIOwords ["ttgerman res2:", res2]
---  text (L.fromStrict res2)
 
 
 
