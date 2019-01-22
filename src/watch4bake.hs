735a997c73eff3909557494a9e501bc2adc7c392
andrew u frank <frank@santafe.local>
2019-01-22 14:31:38 +0100

    split of the watch to trigger bake ops

---
+++ src/watch4bake.hs
@@ -0,0 +1,78 @@
+
+------------------------------------------------------------------------------
+--
+-- Module      :   serving the baked site
+        -- started from treetagger where i used it with post -
+
+-----------------------------------------------------------------------------
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeSynonymInstances  #-}
+{-# LANGUAGE OverloadedStrings     #-}
+
+module Main
+     where
+
+import Uniform.Strings
+import Uniform.Filenames
+import Uniform.FileStrings
+import Uniform.Error
+import Uniform.Convenience.StartApp
+
+import Web.Scotty
+import Network.Wai.Middleware.Static
+
+import System.Directory
+import System.IO
+
+import  Twitch hiding (Options, log)
+import qualified Twitch
+--import Filesystem.Path.CurrentOS
+
+import Lib.Foundation
+import Lib.Bake (bake)
+
+programName = "SSG" :: Text
+progTitle = unwords' ["continuously constructing a static site generator"
+                , "on port ", showT bakedPort]  :: Text
+
+
+mydef = Twitch.Options
+    { Twitch.log                       = NoLogger
+    , logFile                   = Nothing
+    , root                      = Nothing
+    , recurseThroughDirectories = True
+    , debounce                  = DebounceDefault
+    , debounceAmount            = 0
+    , pollInterval              = 10^(6 :: Int) -- 1 second
+    , usePolling                = False
+    }
+main = do
+    putIOwords [programName, progTitle]
+    Twitch.defaultMainWithOptions (mydef
+                    {Twitch.root = Just . toFilePath $ doughPath
+                     , Twitch.log = Twitch.NoLogger
+                    }) $ do
+            Twitch.addModify (\filePath -> runErrorVoid bake) "*.md"     -- add and modify event
+                --  "*.html" |> \_ -> system $ "osascript refreshSafari.AppleScript"
+
+
+runErrorRepl :: (Show a) => a -> IO ()
+-- just for testing when an event is triggered
+runErrorRepl a = do
+                    putIOwords ["runErrorVoid", "input is", showT a]
+                    return ()
+
+runErrorVoid :: ErrIO () -> IO ()
+runErrorVoid a = do
+                    res <- runErr a
+                    putIOwords ["runErrorVoid", showT res]
+                    case res of
+                        Left msg -> error (t2s msg)
+                        Right _ -> return ()
+
+
+
