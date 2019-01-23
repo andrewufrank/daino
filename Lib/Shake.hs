e17f463b065885c1f58cc9af69fe0a51a20c6cd6
andrew u frank <frank@santafe.local>
2019-01-23 15:05:26 +0100

    not clear how prefixes are determined. output is not working

--- Lib/Shake.hs
+++ Lib/Shake.hs
@@ -51,17 +51,19 @@ shake   = do
 
     return ()
 
-bakedD  = toFilePath bakedPath
+bakedD  = "site/baked" -- toFilePath bakedPath
+doughD = "site/dough"
 
 shakeWrapped :: ErrIO  ()
 shakeWrapped = callIO $ shakeArgs shakeOptions {shakeFiles=bakedD } $
     do
-        want ["index"<.> "html"]
+        want ["index"<.>"html"]
 
         "index"<.>"html" %> \out ->
             do
-                mds <- getDirectoryFiles (toFilePath doughPath) ["//*.md"] -- markdown ext ??
+                mds <- getDirectoryFiles  doughD ["//*.md"] -- markdown ext ??
                 let htmlFile = [bakedD </> md -<.> "html" | md <- mds]
+                putIOwords ["shakeWrapped - htmlFile", showT htmlFile]
                 need htmlFile
 --                liftIO $ mapM_ bakeOneFileIO  c
 
@@ -76,12 +78,13 @@ instance Exception Text
 bakeOneFileIO :: FilePath -> IO ()
 bakeOneFileIO fp = do
             et <- runErr $ do
-                            putIOwords ["bakeOneFileIO - from shake xx", s2t fp]
-                            let fp1 = "/"<>fp
-                            putIOwords ["bakeOneFileIO - gp1", s2t fp1]
-                            let fp2 = makeAbsFile fp1
-                            putIOwords ["bakeOneFileIO - next run shake", showT fp2]
---                            bakeOneFileVoid fp
+                    putIOwords ["bakeOneFileIO - from shake xx", s2t fp] --baked/SGGdesign/Principles.md
+
+--                            let fp1 = "/"<>fp
+                    putIOwords ["bakeOneFileIO - gp1", s2t fp1]
+                    let fp2 = makeRelFile fp1
+                    putIOwords ["bakeOneFileIO - next run shake", showT fp2]
+                    bakeOneFileVoid fp1
             case et of
                 Left msg -> throw msg
                 Right _ -> return ()
