708873015b4e384f6240d24dcf95b03f8341722c
andrew u frank <frank@santafe.local>
2019-03-22 12:23:23 +0100

    fixed readSettingsFile (format)

--- Lib/ReadSettingFile.hs
+++ Lib/ReadSettingFile.hs
@@ -1,34 +1,29 @@
-
 ------------------------------------------------------------------------------
 --
 -- Module      :   read the setting file
 --
-
 -----------------------------------------------------------------------------
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeFamilies          #-}
--- {-# LANGUAGE TypeSynonymInstances  #-}
-{-# LANGUAGE OverloadedStrings     #-}
--- {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-module Lib.ReadSettingFile   -- (openMain, htf_thisModuelsTests)
+module Lib.ReadSettingFile -- (openMain, htf_thisModuelsTests)
                            where
 
--- import           Control.Lens                   ( (^?) )
--- import           Data.Aeson
---import Uniform.FileStrings
--- import           Data.Aeson.Lens
--- import           Data.Yaml                      ( decodeThrow )
--- import           Lib.FileMgt
 import           Lib.Foundation -- (getMeta)
---import Text.Pandoc.Definition
 import           Lib.Pandoc
-import Uniform.Yaml (decodeThrowT, YamlText(..), yamlFileType)
-import Uniform.Json (Value, getAt2Key, getAtKey)
--- import Lib.YamlBlocks
+import           Uniform.Json                   ( Value
+                                                , getAt2Key
+                                                , getAtKey
+                                                )
+import           Uniform.Yaml                   ( YamlText(..)
+                                                , decodeThrowT
+                                                , yamlFileType
+                                                )
+
 import           Uniform.Filenames
 import           Uniform.Strings         hiding ( (</>) )
 import           Uniform.TypedFile
@@ -37,44 +32,34 @@ readSettings :: Path Rel File -> ErrIO (SiteLayout, Int)
 -- must be the settings2.yaml file, relative to the current working dir
 -- which contain the rest of the settings
 -- returns layout and port
-
 readSettings settingsfilename = do
     let debug = False
-
---    when debug $
     putIOwords ["readSettings", "file", showT settingsfilename]
     wd          <- currentDir
     settingsTxt <- read8 (wd </> settingsfilename) yamlFileType
     when debug $ putIOwords ["readSettings text", showT settingsTxt]
-    -- TODO where is settings
+      -- TODO where is settings
     layout3 <- readSettings2 debug settingsTxt
---    putIOwords ["readSettings layout3", showT layout3]
     when debug $ putIOwords ["readSettings end", showT layout3]
     return layout3
 
-
+--    when debug $
+--    putIOwords ["readSettings layout3", showT layout3]
 readSettings2 :: Bool -> YamlText -> ErrIO (SiteLayout, Int)
 -- ^ process the settings file
 readSettings2 debug (YamlText t) = do
     meta2 :: Value <- decodeThrowT t -- decodeThrow . t2b $ t
-
     when debug $ putIOwords ["readSettings2 settings", showT meta2]
---    let meta2 = flattenMeta (getMeta pandoc)
-
---    let themeDir2 = meta2 ^?   key "themeDir" . _String :: Maybe Text
---    when debug $ putIOwords ["readSettings2 themedir", showT themeDir2]
-
-    let themeDir2 = getAt2Key meta2 "storage"   "themeDir" :: Maybe Text
-        --     meta2 ^? key "storage" . key "themeDir" . _String :: Maybe Text
-    let doughDir2 = getAt2Key meta2 "storage"   "doughDir"  :: Maybe Text
-    let bakedDir2 = getAt2Key meta2
-              "storage"   "bakedDir"   :: Maybe Text
-    let reportFile2 = getAt2Key meta2
-              "storage"   "reportFile"  :: Maybe Text
-    let testDir2 = getAt2Key meta2  "storage"  "testDir"  :: Maybe Text
-    let port = getAtKey meta2  "localhostPort"   :: Maybe Integer
-
-
+  --    let meta2 = flattenMeta (getMeta pandoc)
+  --    let themeDir2 = meta2 ^?   key "themeDir" . _String :: Maybe Text
+  --    when debug $ putIOwords ["readSettings2 themedir", showT themeDir2]
+    let themeDir2   = getAt2Key meta2 "storage" "themeDir" :: Maybe Text
+          --     meta2 ^? key "storage" . key "themeDir" . _String :: Maybe Text
+    let doughDir2   = getAt2Key meta2 "storage" "doughDir" :: Maybe Text
+    let bakedDir2   = getAt2Key meta2 "storage" "bakedDir" :: Maybe Text
+    let reportFile2 = getAt2Key meta2 "storage" "reportFile" :: Maybe Text
+    let testDir2    = getAt2Key meta2 "storage" "testDir" :: Maybe Text
+    let port        = getAtKey meta2 "localhostPort" :: Maybe Integer
     let
         layout3 = SiteLayout
             { themeDir   = makeAbsDir . t2s $ fromJustNote "themedir xxdwe"
@@ -87,16 +72,12 @@ readSettings2 debug (YamlText t) = do
                                                             reportFile2
             , testDir = makeAbsDir . t2s $ fromJustNote "testdir xxdwe" testDir2
             }
---    let layout3 = F.layoutDefaults
+  --    let layout3 = F.layoutDefaults
     let port2 = fromInteger . fromJustNote "port wrwer" $ port
-
     when debug $ putIOwords ["readSettings2", showT layout3]
---    when debug $
+  --    when debug $
     when debug $ putIOwords ["readSettings2 port", showT port2]
-
-    return (layout3, port2)
-
---instance FromJSON Settings
+    return (layout3, port2)--instance FromJSON Settings
 --
 --data Settings = Settings
 --    { themeDir :: Text -- ^ the place of the  theme files (includes templates)
@@ -106,12 +87,3 @@ readSettings2 debug (YamlText t) = do
 --    , testDir :: Text -- ^ the directory the test results go
 --                        -- not important
 --    } deriving (Show, Generic)
-
-
-
-
-
-
-
-
-
