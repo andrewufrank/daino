28d660c1663e5449529ab3571d9ef6dce29c1d16
Andrew U Frank <frank@geoinfo.tuwien.ac.at>
2020-05-31 21:14:31 +0200

    clean up

--- Lib/Bake.hs
+++ Lib/Bake.hs
@@ -50,42 +50,31 @@ bakeOneFile
 bakeOneFile debug flags pageFn layout ht2 =
   do
       putIOwords ["\n-----------------", "bakeOneFile fn", showT pageFn, "debug", showT debug]
-                         -- currently only for md files
-                 --        pageMd :: MarkdownText <- read8 pageFn markdownFileType -- pageFn -> pageMd
-                         -- process the md file (including bibtex citations)
-                 --        let resourcesPath = doughP </> resourcesDirName :: Path Abs Dir
       (pandoc, metaRec, report) <- getTripleDoc layout pageFn
       -- how are errors dealt with 
       -- let debug = True
+
       pandoc2 :: Pandoc <- markdownToPandocBiblio debug flags (doughDir layout) (pandoc, metaRec, report) -- AG -> AD
                         -- withSettings.pandoc
                          -- produce html and put into contentHtml key
                          -- can be nothing if the md file is not ready to publish
-      -- case mtripledoc of
-      --   Just tripleDoc -> do
       htmlout :: HTMLout <- pandocToContentHtml debug pandoc2 -- content.docval  AD -> AF
-    --   index :: MenuEntry <- makeIndex debug flags metaRec
-    --   putIOwords ["bakeOneFile index", showT index, "\n"]
 
       val    <- docValToAllVal debug layout flags htmlout  metaRec
       -- includes the directory list and injection, which should be in 
       -- value "menu2"
       html2  <- putValinMaster debug val (templatesDir layout)
       write8 ht2 htmloutFileType html2
-        --            putIOwords ["bakeOneFile outhtml
-        --            (which was just written) \n", unHTMLout html2, "\n"]
-      when debug $  putIOwords
+    
+       when debug $  putIOwords
             ["bakeOneFile resultFile", showT ht2, "from", showT pageFn, "\n"]
       when debug $ putIOwords
             ["bakeOneFile resultvalue", showT val, "\n"
                 , showT html2]--   when debug $ 
       putIOwords ["......................"]
       return . unwords' $ ["bakeOneFile outhtml ", showT pageFn, "done"]
-        -- Nothing ->
-        --   return
-        --     . unwords'
-        --     $ ["bakeOneFile outhtml ", showT pageFn, "nothing to do"]
-    `catchError` (\e -> do
+
+        `catchError` (\e -> do
                    let errmsg2 =
                          [ "\n****************"
                          , "bakeOneFile catchError"
