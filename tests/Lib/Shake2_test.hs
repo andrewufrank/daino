f3604b3233b66ff51b0004b0b2a3c9e6035512c1
Andrew U Frank <frank@geoinfo.tuwien.ac.at>
2020-06-02 11:18:37 +0200

    start with 0.0.3.0 - problem: only static files are baked

--- tests/Lib/Shake2_test.hs
+++ tests/Lib/Shake2_test.hs
@@ -26,17 +26,28 @@ import Lib.Foundation_test (testLayout)
 import Lib.Foundation (templatesImgDirName, bannerImageFileName)
 import Lib.CmdLineArgs (allFlags)
 
+    -- tests a conversion, is always ok
+    --  "Lib.Shake_test - files written to /home/frank/.SSG/bakedTest"
 test_shakeMD = 
     do 
         res <- shakeMD testLayout allFlags
                 (doughDir testLayout)
                 ((themeDir $ testLayout) `addFileName` ( templatesDirName)
                             :: Path Abs Dir)
-                (makeAbsDir "/home/frank/.SSG/bakedTest")
+                (makeAbsDir "/home/frank/.SSG/bakedTest0")
                 (templatesImgDirName `addFileName` bannerImageFileName)
         assertEqual res ()
-        --  "Lib.Shake_test - files written to /home/frank/.SSG/bakedTest"
 
+-- shakeOp :: Text -> ErrIO Text 
+--         -- SiteLayout -> PubFlags -> FilePath -> ErrIO ()
+-- shakeOp _ = do 
+--     putIOwords ["shake with layout", showT testLayout]
+--     shakeAll testLayout allFlags "" 
+--     -- let bakedP = bakeDir testLayout
+--     -- ex
+--     -- res <- 
+-- test_shakeAll = testVar0FileIO "shake" "shake" "shakeTest" shakeOp  
+    -- testVar0FileIO progName  a resfile op
 -- doughP = makeAbsDir "/home/frank/Workspace8/ssg/docs/site/dough"
 
 -- test_directoryToBake_null = do 
