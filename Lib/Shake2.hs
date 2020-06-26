2ff5ec1151b7132f0c829a1c780328d800eedab2
andrew u frank <frank@santafe.local>
2020-06-26 15:47:08 +0200

    all files converted. imgs copied

--- Lib/Shake2.hs
+++ Lib/Shake2.hs
@@ -136,7 +136,7 @@ shakeMD debug layout flags doughP bakedP = shakeArgs2 bakedP $ do
         -- which then produce them
         -- the original start needs in baked (from the files in dough)  
         pdfs  <- getNeeds debug doughP bakedP "md" "pdf"
-        htmls      <- getNeeds debug  doughP bakedP "md" "html"
+        htmls <- getNeeds debug  doughP bakedP "md" "html"
 
                 -- templatesP 
                 -- (bakedP </> staticDirName) -- exception
@@ -147,16 +147,17 @@ shakeMD debug layout flags doughP bakedP = shakeArgs2 bakedP $ do
         -- but produce the common precursor docval
         -- TODO 
 
-        bibs         <- getNeeds debug doughP bakedP "bib" "bib"
-        imgs          <- getNeeds debug doughP bakedP "jpg" "jpg"
-        imgs2         <- getNeeds debug doughP bakedP "JPG" "JPG"
+        -- bibs         <- getNeeds debug doughP bakedP "bib" "bib"
+        -- imgs          <- getNeeds debug doughP bakedP "jpg" "jpg"
+        -- imgs2         <- getNeeds debug doughP bakedP "JPG" "JPG"
 
-        csss          <- getNeeds debug doughP bakedP "css" "css"
+        -- csss          <- getNeeds debug doughP bakedP "css" "css"
+        -- csls          <- getNeeds debug doughP bakedP "csl" "csl"
 
-        needP bibs
-        needP imgs
-        needP imgs2
-        needP csss
+        -- needP bibs
+        -- needP imgs
+        -- needP imgs2
+        -- needP csss
 
         needP pdfs
         needP htmls
@@ -168,15 +169,26 @@ shakeMD debug layout flags doughP bakedP = shakeArgs2 bakedP $ do
     (toFilePath bakedP <> "**/*.html")
         %> \out
         -- calls the copy html and the conversion from md
-                -> convertAny debug bakedP bakedP flags layout out convDocrep2html
+                -> do 
+                    csss          <- getNeeds debug doughP bakedP "css" "css"
+                    needP csss 
+                    putIOwords ["rule **/*.html need", showT csss]
+                    imgs          <- getNeeds debug doughP bakedP "jpg" "jpg"
+                    imgs2         <- getNeeds debug doughP bakedP "JPG" "JPG"
+                    needP imgs
+                    needP imgs2
+                    putIOwords ["rule **/*.html need", showT imgs, showT imgs2]
+                    convertAny debug bakedP bakedP flags layout out convDocrep2html
 
     (toFilePath bakedP <> "**/*.pdf")
         %> \out -- insert pdfFIles1  
                 -> do 
                     putIOwords ["rule **/*.pdf", showT out]
+                    imgs          <- getNeeds debug doughP bakedP "jpg" "jpg"
                     imgs2         <- getNeeds debug doughP bakedP "JPG" "JPG"
+                    needP imgs
                     needP imgs2
-                    putIOwords ["rule **/*.pdf need", showT imgs2]
+                    putIOwords ["rule **/*.pdf need", showT imgs, showT imgs2]
                     convertAny debug2 bakedP bakedP flags layout out convTex2pdf
 
     (toFilePath bakedP <> "**/*.tex")
@@ -189,7 +201,15 @@ shakeMD debug layout flags doughP bakedP = shakeArgs2 bakedP $ do
 
     (toFilePath bakedP <> "**/*.docrep")
         %> \out -- insert pdfFIles1  -- here start with doughP
-                -> convertAny debug2 doughP bakedP flags layout out convMD2docrep
+                -> do 
+                    bibs         <- getNeeds debug doughP bakedP "bib" "bib"
+                    needP bibs
+                    csls          <- getNeeds debug doughP bakedP "csl" "csl"
+                    needP csls 
+                    putIOwords ["rule **/*.docrep need", showT bibs]
+                    putIOwords ["rule **/*.docrep need", showT csls]
+
+                    convertAny debug2 doughP bakedP flags layout out convMD2docrep
 
     -- rest are copies 
 
--- docs/site/dough/Blog/postwk.md
+++ docs/site/dough/Blog/postwk.md
@@ -11,6 +11,8 @@ Some text without meaning; really? with an image
 
 ![first picture ]( /home/frank/Workspace8/ssg/docs/site/dough/resources/img/120-2026_IMG.JPG  "Logo Title Text 1")
 
+ <!-- ![first picture ]( ../resources/img/120-2026_IMG.JPG  "Logo Title Text 1")  -->
+
 absolute statt relative path "/resources/img/120-2026_IMG.JPG"
   An example post.  postwk but in blog - not in SubBlog
 with some additional text 
