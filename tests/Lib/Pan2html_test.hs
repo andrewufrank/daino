4d70692ed8d292d3201a526d8e6ddf35eb4cce88
Andrew U. Frank <frank@geoinfo.tuwien.ac.at>
2021-05-21 15:42:57 +0200

    before cleaning

--- tests/Lib/Pan2html_test.hs
+++ tests/Lib/Pan2html_test.hs
@@ -36,18 +36,18 @@ op :: Panrep  -> IndexEntry
 op = dyIndexEntry . panyam
  
 test_pan2indexEntry_blog1 = testing_pan2indexEntry "blog1"
--- test_pan2indexEntry_index = testing_pan2indexEntry "index"
--- test_pan2indexEntry_postwk = testing_pan2indexEntry "postwk"
--- test_pan2indexEntry_withRef = testing_pan2indexEntry "withRef"
+test_pan2indexEntry_index = testing_pan2indexEntry "index"
+test_pan2indexEntry_postwk = testing_pan2indexEntry "postwk"
+test_pan2indexEntry_withRef = testing_pan2indexEntry "withRef"
 
 testing_pan2HTMLout f = test1FileIO "ssg"  (  "panrep_" <> f) ("htmlout_" <> f) op1        -- dr1 <- read8 inputFn docrepFileType
 op1 :: Panrep  -> ErrIO HTMLout 
 op1 = panrep2html NoticeLevel0 settings403
  
 test_pan2HTMLout_blog1 = testing_pan2HTMLout "blog1"
--- test_pan2HTMLout_index = testing_pan2HTMLout "index"
--- test_pan2HTMLout_postwk = testing_pan2HTMLout "postwk"
--- test_pan2HTMLout_withRef = testing_pan2HTMLout "withRef"
+test_pan2HTMLout_index = testing_pan2HTMLout "index"
+test_pan2HTMLout_postwk = testing_pan2HTMLout "postwk"
+test_pan2HTMLout_withRef = testing_pan2HTMLout "withRef"
 
 -- -- | conversion of markdown file f1 (with extension) to intermediate d11  
 -- testing_md2dr1 f1 = test1FileIO "ssg"  (f1<> ".md") (f1 <> "_dr1" )  (readMarkdown2docrep NoticeLevel0 doughP bakedP fn2process .  MarkdownText) 
