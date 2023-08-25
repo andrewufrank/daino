f970bc4940ea47e367a9cba390da0bf6485ee48a
Andrew U. Frank <frank@geoinfo.tuwien.ac.at>
2023-08-25 16:49:11 +0200

    umgesteellt in bake.hs

--- src/ShakeBake/Bake.hs
+++ src/ShakeBake/Bake.hs
@@ -42,6 +42,90 @@ type BakeOp =
     Path Abs File ->
     ErrIO [FilePath] -- additional needs found 
 
+getNeeds4html :: BakeOp
+getNeeds4html debug flags bakedFrom sett3 outP = do 
+    when (inform debug) $    putIOwords
+        [ "\n-----"
+        , "getNeeds4html 1"
+        , showT outP
+        , "\n    bakedFrom"
+        , showT bakedFrom
+        ]
+
+    return [toFilePath bakedFrom]
+
+
+bakeOnePanrep2html :: BakeOp -- PANREP -> HTML  -- TODO
+bakeOnePanrep2html debug flags inputFn sett3 resfn2 = do
+    when (inform debug) $    putIOwords
+        [ "\n-----------------"
+        , "bakeOnePanrep2html 1 fn"
+        , showT inputFn
+        , "\n resfn2"
+        , showT resfn2
+        ]
+    dr1 <- read8 inputFn panrepFileType
+ 
+
+    (p, needsFound, test_templatehtml) <- panrep2html debug  flags dr1
+
+    write8 resfn2 htmloutFileType ( p) -- content is html style
+    write8 resfn2 tthFileType test_templatehtml
+    -- write the test for filling the template always 
+    when (inform debug) $
+        putIOwords
+            ["\n-----------------", "bakeOnePanrep2html done fn", showT resfn2]
+    return  needsFound
+
+getNeeds4pan :: BakeOp
+getNeeds4pan debug flags bakedFrom sett3 outP = do 
+    when (inform debug) $    putIOwords
+        [ "\n-----"
+        , "getNeeds4pan 1"
+        , showT outP
+        , "\n   bakedFrom"
+        , showT bakedFrom
+        ]
+
+    return [toFilePath bakedFrom]
+
+bakeOneDocrep2panrep :: BakeOp --  DOCREP -> PANREP
+--   add index  
+bakeOneDocrep2panrep debug flags inputFn sett3 resfn2 = do
+    -- let debug = NoticeLevel2
+    when (inform debug) $    putIOwords
+        [ "-----------------"
+        , "bakeOneDocrep2panrep 1 inputFn"
+        , showT inputFn
+        , showT resfn2
+        ]
+    dr1 <- read8 inputFn docrepFileType
+
+    -- let layout = siteLayout sett3
+    (p3, needsFound) <- docrep2panrep debug flags  dr1
+            -- completes index and should process reps 
+            -- what to do with needs?
+
+
+    write8 resfn2 panrepFileType p3 -- content is html style
+    putInform debug 
+            ["\n-----------------", "bakeOneDocrep2panrep done produced resf2n", showT resfn2
+                , "\n needsFound", showT needsFound]
+    return   needsFound
+        -- these needs were not tested for version >= publish
+
+getNeeds4doc :: BakeOp
+getNeeds4doc debug flags bakedFrom sett3 outP = do 
+    when (inform debug) $    putIOwords
+        [ "\n-----"
+        , "getNeeds4doc 1"
+        , showT outP
+        , "\n   bakedFrom"
+        , showT bakedFrom
+        ]
+
+    return [toFilePath bakedFrom]
+    
 bakeOneMD2docrep :: BakeOp --    MD -> DOCREP
 -- process the md to pandoc format (parser)
 -- and add the refs 
@@ -69,86 +153,4 @@ bakeOneMD2docrep debug flags inputFn sett3 resfn2 = do
             ]
     return []
 
-bakeOneDocrep2panrep :: BakeOp --  DOCREP -> PANREP
---   add index  
-bakeOneDocrep2panrep debug flags inputFn sett3 resfn2 = do
-    -- let debug = NoticeLevel2
-    when (inform debug) $    putIOwords
-        [ "-----------------"
-        , "bakeOneDocrep2panrep 1 inputFn"
-        , showT inputFn
-        , showT resfn2
-        ]
-    dr1 <- read8 inputFn docrepFileType
 
-    -- let layout = siteLayout sett3
-    (p3, needsFound) <- docrep2panrep debug flags  dr1
-            -- completes index and should process reps 
-            -- what to do with needs?
-
-
-    write8 resfn2 panrepFileType p3 -- content is html style
-    putInform debug 
-            ["\n-----------------", "bakeOneDocrep2panrep done produced resf2n", showT resfn2
-                , "\n needsFound", showT needsFound]
-    return   needsFound
-        -- these needs were not tested for version >= publish
-
-
-getNeeds4html :: BakeOp
-getNeeds4html debug flags bakedFrom sett3 outP = do 
-    when (inform debug) $    putIOwords
-        [ "\n-----"
-        , "getNeeds4html 1"
-        , showT outP
-        , "\n    bakedFrom"
-        , showT bakedFrom
-        ]
-
-    return [toFilePath bakedFrom]
-
-getNeeds4pan :: BakeOp
-getNeeds4pan debug flags bakedFrom sett3 outP = do 
-    when (inform debug) $    putIOwords
-        [ "\n-----"
-        , "getNeeds4pan 1"
-        , showT outP
-        , "\n   bakedFrom"
-        , showT bakedFrom
-        ]
-
-    return [toFilePath bakedFrom]
-
-getNeeds4doc :: BakeOp
-getNeeds4doc debug flags bakedFrom sett3 outP = do 
-    when (inform debug) $    putIOwords
-        [ "\n-----"
-        , "getNeeds4doc 1"
-        , showT outP
-        , "\n   bakedFrom"
-        , showT bakedFrom
-        ]
-
-    return [toFilePath bakedFrom]
-
-bakeOnePanrep2html :: BakeOp -- PANREP -> HTML  -- TODO
-bakeOnePanrep2html debug flags inputFn sett3 resfn2 = do
-    when (inform debug) $    putIOwords
-        [ "\n-----------------"
-        , "bakeOnePanrep2html 1 fn"
-        , showT inputFn
-        , "\n resfn2"
-        , showT resfn2
-        ]
-    dr1 <- read8 inputFn panrepFileType
- 
-
-    (p, needsFound, test_templatehtml) <- panrep2html debug  flags dr1
-
-    write8 resfn2 htmloutFileType ( p) -- content is html style
-    write8 resfn2 tthFileType test_templatehtml
-    -- write the test for filling the template always 
-    when (inform debug) $
-        putIOwords
-            ["\n-----------------", "bakeOnePanrep2html done fn", showT resfn2]
-    return  needsFound
