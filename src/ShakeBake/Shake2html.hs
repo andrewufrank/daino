f49eb98406a03e92b007bd18389b5f75f7861f8b
Andrew U. Frank <frank@geoinfo.tuwien.ac.at>
2023-08-26 23:58:23 +0200

    cleaned

--- src/ShakeBake/Shake2html.hs
+++ src/ShakeBake/Shake2html.hs
@@ -80,19 +80,11 @@ shake2html debug flags sett4 bakedP  =
             --  bakeOnePanrep2html debug flags bakedFrom sett4 outP 
             -- bakeOnePanrep2html debug flags inputFn sett3 resfn2 = do
         when (inform debug) $    putIOwords
-                [ "\n-----------------"
-                , "bakeOnePanrep2html 1 fn"
-                , showT bakedFrom
-                , "\n resfn2"
-                , showT outP
+                [ "\n-----\nrule **/*.html 1 fn", showT bakedFrom
+                , "\n                    resfn2", showT outP
                 ]
         (pan0) <- runErr2action $  read8 bakedFrom panrepFileType
         
-
-        -- (p, needsFound3, test_templatehtml) <- 
-        --  panrep2html debug  flags pan0
-        -- panrep2html debug flags  metaplus4 = do
-        -- let debug = NoticeLevel0   -- avoid_output_fromHere_down
         let sett3 = sett pan0
             extra4 = extra pan0
             mf = masterTemplateFile $ siteLayout sett3
@@ -101,22 +93,19 @@ shake2html debug flags sett4 bakedP  =
 
     -- braucht needs fuer die panrep files
 
-                -- the rel path to index files
         let ixs0pan = map (addFileName bakedP 
                             . addExtension extPanrep) ixs0 :: [Path Abs File]
-        putInform debug ["panrep2html", "needs panrep ixs0pan", showT ixs0pan]
+        putInform debug ["\n-----\nrule **/*.html", "needs panrep ixs0pan", showT ixs0pan]
         
         needP ixs0pan
 
-        putInform debug["\npanrep2html", "siteLayout sett3"
+        putInform debug["\n-----\nrule **/*.html", "siteLayout sett3"
                     , showPretty $ siteLayout sett3]
-        putInform debug ["panrep2html", "mf", showPretty mf]
-        putInform debug ["panrep2html", "masterfn", showPretty masterfn]
+        putInform debug ["\n-----\nrule **/*.html", "mf", showPretty mf]
+        putInform debug ["\n-----\nrule **/*.html", "masterfn", showPretty masterfn]
 
         targetTempl  <- runErr2action $ compileTemplateFile2 masterfn
         testTempl  <- runErr2action $ compileTemplateFile2 testTemplateFn
-
-            -- htm1 <- meta2xx writeHtml5String2 (metap pan0)
 
             --if this is an index file it has files and dirs 
             -- putInform debug ["panrep2html", "extra4", showPretty extra4]
@@ -124,17 +113,13 @@ shake2html debug flags sett4 bakedP  =
         let files = fileEntries  $ extra4 :: [IndexEntry2]
             dirs = dirEntries  $ extra4 :: [IndexEntry2]
 
-            -- let bakedP =   bakedDir . siteLayout $ sett3
-        
- 
-
         valsDirs :: [Maybe IndexEntry2]<- runErr2action $ mapM 
                             (getVals2html debug flags bakedP) dirs
         valsFiles :: [Maybe IndexEntry2] <- runErr2action $ mapM 
                         (getVals2html debug flags bakedP) files
 
-        putInform debug["panrep2html", "valsDirs", showPretty valsDirs]
-        putInform debug ["panrep2html", "valsFiles", showPretty valsFiles]
+        putInform debug["\n-----\nrule **/*.html", "valsDirs", showPretty valsDirs]
+        putInform debug ["\n-----\nrule **/*.html", "valsFiles", showPretty valsFiles]
 
         let extra5 = extra4{fileEntries = catMaybes valsFiles
                             , dirEntries = catMaybes valsDirs}
@@ -143,31 +128,24 @@ shake2html debug flags sett4 bakedP  =
             -- putInform debug ["panrep2html", "extra5", showPretty extra5]
             -- putInform debug ["panrep2html", "metaplus5", showPretty metaplus5]
 
-            -- let hpl1 = renderTemplate targetTempl (toJSON metaplus5)  -- :: Doc Text
         let ht1 = fillTemplate_render targetTempl metaplus5
-
-            -- let ttpl1 = renderTemplate testTempl (toJSON metaplus5)  -- :: Doc Text
         let test_templatehtml = fillTemplate_render testTempl metaplus5 
 
             -- putInform debug ["panrep2html render html done", "ht1",  ht1 ]
             -- putInform debug ["panrep2html render testTemplate done", "test_templatehtml",  test_templatehtml ]
 
-            -- bakeOnePanrep2html will write to disk
-            -- return (HTMLout ht1, [], tt1) -- needs are dealt with so far above
-            -- let (p, needsFound3, test_templatehtml) = (HTMLout ht1, [], tt1)
         runErr2action $ do 
             write8 outP htmloutFileType ( HTMLout ht1) 
             write8 outP tthFileType test_templatehtml
 
         when (inform debug) $
             putIOwords
-                ["\n-----------------", "bakeOnePanrep2html done fn", showT outP]
+                ["\n-----\nrule **/*.html", "bakeOnePanrep2html done fn", showT outP]
 
-        putInform debug ["\nrule **/*.html end continued 4", showT out]
+        putInform debug ["\n-----\nrule **/*.html end continued 4", showT out]
 
 
 fillTemplate_render  tpl dat = render (Just 50)
         -- just a plausible line length of 50 
         $  renderTemplate tpl (toJSON dat)
-    -- let ttpl1 = renderTemplate testTempl (toJSON metaplus5)  -- :: Doc Text
-    --         let test_templatehtml = render (Just 50) ttpl1  -- line length, can 
+ 
